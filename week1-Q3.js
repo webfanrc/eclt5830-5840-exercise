f3f3195c17d270b9e3bbd5f9ea3d188369d05d35
RuoChen95 <xrc_1995@163.com>
2020-09-19 17:05:32 +0800

    修改week1-Q3

--- week1-Q3.js
+++ week1-Q3.js
@@ -10,29 +10,49 @@ number of digits in its integer portion. (e.g., for -456.99, the integer
 portion is -456, so return 3)
 - Otherwise, return 0
 
+
+---
+With your current implementation,
+length({}) yield undefined instead of 0.
+=> switch的一个问题
+
+length(null) raises an exception instead of returning 0.
+=> switch的一个问题
+
+length(9) yields 2 instead of 1.
+=> 修改function
+
+Also, Infinity can be positive and negative. There is a Math function that can check all Infinity and NaN.
+=> isFinite()
  */
 
+
+
+
+//用if else更好一点
 function length(par) {
   switch (typeof par) {
     case 'object':
       if (Array.isArray(par)) return par.length;
+      else return 0;
     case 'string':
       return par.length;
     case 'number':
-      if (par !== Infinity && !isNaN(par)) return getDigits(par);
+      if (isFinite(par)) return getDigits(par);
+      else return 0;
     default:
-      return 0
+      return 0;
   }
 }
 
 function getDigits(num) {
-  let result = 1;
-  num = Math.abs(Math.trunc(num));
-   while (num > 1) {
-     num = num/10;
-     result++;
-   }
- return result;
+  num = Math.abs(Math.floor(num));
+  //  let result = 0;
+  //  while (num > 0) {
+  //    num = Math.trunc(num/10);
+  //    result++;
+  //  }
+  return num.toString().length;
 }
 
-console.log(length(-100.123));
+console.log(length(20000.12));
