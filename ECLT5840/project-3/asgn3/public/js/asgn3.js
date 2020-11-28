bdbb134445946a93b00a88e1e7f384b880c224cb
RuoChen95 <xrc_1995@163.com>
2020-11-28 15:58:58 +0800

    assign3 - task3

--- ECLT5840/project-3/asgn3/public/js/asgn3.js
+++ ECLT5840/project-3/asgn3/public/js/asgn3.js
@@ -9,7 +9,6 @@ window.addEventListener('load', function() {
     - Use this methods to set the items in the list.
 */
 
-// TODO: Complete the implementation of these four functions.
 let asgn3 = {
   // app作为Vue实例的引用
   retrieveItems: function(app) {
@@ -29,6 +28,7 @@ let asgn3 = {
 
     // 用户直接选id而不是index
     localStorage.setItem('selectedItems', app.selected);
+    console.log("saved");
   },
 
   restoreSelection: function(app) {
@@ -40,12 +40,15 @@ let asgn3 = {
     // Task 3
     // TODO: Send app.selected via Ajax to "server B" and
     //   output the array in the response to the JS console.
-    // (See Task 3's requirements for more info)
-
-    // You need to implement the server-side script accordingly.
-
-    // Remove this statement when you have implemented this function.
-    window.alert('submitSelection() has not yet been implemented');
+    $.ajax({
+      url: 'http://localhost:8081/sendSelection',
+      type: 'POST',
+      data: app.selected.toString(),
+      async: true,
+      success: function(result) {
+        console.log(result);
+      }
+    });
   }
 }
 
