213f9bf0a0be98d776e4392863ccc4badd4346d0
RuoChen95 <xrc_1995@163.com>
2020-12-04 15:04:26 +0800

    5830:assign3-1（优化

--- ECLT5830/assign3-Hash/assign1.js
+++ ECLT5830/assign3-Hash/assign1.js
@@ -1,8 +1,8 @@
-const String = 'ECIT';
+const String = 'A';
 
 
-// input: String
-// function: reverse
+// input: Number
+// function: reverse binary string
 // e.g.: 01010011=>11001010
 function f(number) {
   let string_2 = parseInt(number).toString(2);
@@ -17,17 +17,29 @@ function f(number) {
   return string_2.split("").reverse().join("");
 }
 
+// let m1 = String[0].charCodeAt();
+// let m2 = String[1].charCodeAt();
+// let m3 = String[2].charCodeAt();
+// let m4 = String[3].charCodeAt();
+//
+// let c1 = parseInt(f(m1),2);
+// let c2 = parseInt(f(c1^m2), 2);
+// let c3 = parseInt(f(c2^m3), 2);
+// let c4 = f(c3^m4);
 
-let m1 = String[0].charCodeAt();
-let m2 = String[1].charCodeAt();
-let m3 = String[2].charCodeAt();
-let m4 = String[3].charCodeAt();
+let c, temp;
+for (let i = 0; i < String.length-1; i++) {
+  let m = String[i].charCodeAt(); // ASCII
+  if (i == 0) {
+    c = parseInt(f(m),2);
+  } else {
+    c = parseInt(f(temp^m), 2);
+  }
+  temp = c;
+}
 
-let c1 = parseInt(f(m1),2);
-let c2 = parseInt(f(c1^m2), 2);
-let c3 = parseInt(f(c2^m3), 2);
-let c4 = f(c3^m4);
+let result = f(temp^String[String.length-1].charCodeAt());
 
 
 
-console.log(c4);
+console.log(result);
